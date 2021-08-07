385ff066e578c1d507feaee5bc287ed7c166d8ad
Krisha Bhambani <62170625+pixiedust18@users.noreply.github.com>
2021-08-08 01:35:16 +0530

    Update Registration.py

--- src/Registration.py
+++ src/Registration.py
@@ -30,7 +30,7 @@ class CNN(object):
         self.beta = 2.0
         self.lambd = 0.5
 
-        self.cnnph = tf.keras.Input([2, 224, 224, 3], dtype=tf.dtypes.float32)
+        self.cnnph = tf.Variable(tf.ones(shape=(2, 224, 224, 3)))
         self.vgg = VGG16mo()
         self.vgg.build(self.cnnph)
         self.SC = ShapeContext()
